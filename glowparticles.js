4395f687acdad9cb9006991d35ea1ed2aff2f506
PoorBuster <110546252+PoorBuster@users.noreply.github.com>
2022-08-27 09:26:21 +0800

    Update glowparticles.js

--- glowparticles.js
+++ glowparticles.js
@@ -48,7 +48,7 @@ export class GlowParticle {
         );
         //g.addColorStop(0, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 1)`);
        //g.addColorStop(1, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 0)`);
-        ctx.fillStye = `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 1)`;
+        ctx.fillStyle = `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 1)`;
         //no gradient style the above line
         //ctx.fillStyle = g;
         ctx.arc(this.x, this.y, this.radius, 0, PI2, false);
