df8205afe4af8af0522398f5fd166c407f68204e
PoorBuster <110546252+PoorBuster@users.noreply.github.com>
2022-08-27 11:19:00 +0800

    Update moving-gradient.js

--- moving-gradient.js
+++ moving-gradient.js
@@ -17,10 +17,10 @@ class App {
 
         this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
 
-        this.totalParticles = 3;
+        this.totalParticles = 15;
         this.particles = [];
-        this.maxRadius = 90;
-        this.minRadius = 40;
+        this.maxRadius = 900;
+        this.minRadius = 400;
         //the larger the number in radius, the more blur
 
         window.addEventListener('resize', this.resize.bind(this), false);
